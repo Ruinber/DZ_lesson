1f689f4638eead88d7d32123584736ee499c00ea
Ruinber <133604409+Ruinber@users.noreply.github.com>
2023-05-29 16:24:56 +0300

    pand

---
+++ panda.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+# series = pd.Series([1, 2, 23, 223], index=["a", "b", "c", "d"])
+# print(series.values)
+# print(series.index)
+
+
+
+# data = pd.DataFrame({'color' : ['blue', 'green', 'yellow', 'red', 'white'],
+#         'object' : ['ball', 'pen', 'pencil', 'paper', 'mug'],
+#         'price' : [1.2, 1.0, 0.6, 0.9, 1.7]})
+# # tabl = pd.DataFrame(data)
+# print(data)
+
+
